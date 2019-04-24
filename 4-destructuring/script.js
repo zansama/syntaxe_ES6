3e0363f4e98a10826118550e345424bfc38807e2
zan <clement.mortas@le-campus-numerique.fr>
2019-04-24 13:45:39 +0200

    4-destructuring

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -1,13 +1,25 @@
 //4.1 Découverte
 
-const [a, b] = [1, 2, 3, 4]
+const [a, b, c] = [1, 2, 3, 4] // affectation des  deux première entrée du tableau -> a et b de la constante  [a , b,]
+// donc a = 1 et b = 2
 console.log(a)
 console.log(b)
+console.log(c)
 
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35}
+
+const {first, last, age} = {first: 'Paul', last: 'Henta', age: 35}
 console.log(first)
 console.log(last)
+console.log(age)
 
 //4.2 Application
 
 console.log(data)
+for (let i = 0 ; i <Object.keys(data).length; i++){
+  const {dateDebut, dateFin, texte} = {
+    dateDebut : Object.values(data)[i].dateDebut,
+    dateFin : Object.values(data)[i].dateFin,
+    texte : Object.values(data)[i].texte,
+  }
+    console.log('A partir de '+ dateDebut +' '+ 'jusqu\'a ' + dateFin + ' ' + texte);
+}
