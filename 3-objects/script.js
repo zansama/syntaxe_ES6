918b27c0c6bf729904b53a97527cf959446e150f
zan <clement.mortas@le-campus-numerique.fr>
2019-04-24 11:56:55 +0200

    3-object

--- 3-objects/script.js
+++ 3-objects/script.js
@@ -12,6 +12,17 @@ const bus = {
     equipments: ['sae', 'tft', 'speech']
 }
 
+console.log(Object.keys(bus));
+console.log(Object.values(bus));
+
+for (let i = 0 ; i <Object.keys(bus).length; i++){
+    console.log(Object.keys(bus)[i], ':', Object.values(bus)[i])
+}
+
+
 //3.2 Object.values
 
-console.log(data)
+for (let i = 0 ; i <Object.keys(data).length; i++){
+    console.log(Object.keys(data)[i], ':', Object.values(data)[i].texte);
+}
+//console.log(data)
